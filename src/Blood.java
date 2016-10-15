831086fbeabf5d3fa6c8b527739a1928319263a4
iamsubhranil <subhranilmukherjee190@gmail.com>
2016-10-15 14:24:04 +0530

    Creation of Blood and Corpusle

---
+++ src/Blood.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+/**
+ * Author : Nil
+ * Date : 10/15/2016 at 2:21 PM.
+ * Project : HumanSystem
+ */
+public class Blood {
+
+    private final ArrayList<Corpusle> whiteCorps = new ArrayList<>();
+
+}
---
+++ src/Corpusle.java
@@ -0,0 +1,7 @@
+/**
+ * Author : Nil
+ * Date : 10/15/2016 at 2:22 PM.
+ * Project : HumanSystem
+ */
+public class Corpusle {
+}
